0e0fb287b0f67c979904f360ef173c4b3f8a3317
William <111627897+codevoid048@users.noreply.github.com>
2025-03-23 21:20:26 +0530

    Backend for basic authentication, Leaderboard, tracking activity of users, Schemas
    * Initial setup
    
    * Base setup of the interfaces
    
    * Update Readme.md
    
    * Landing Page updated
    
    * Backend for
        - basic authentication
        - protected API calls
        - Leaderboard
        - Schema evaluation
        - Activities tracking
    
    * Resolved backend logs issues
    
    Co-authored-by: Vigna Ramtej Telagarapu <166812260+ramtejvigna@users.noreply.github.com>

--- server/server.js
+++ server/server.js
@@ -1,15 +1,11 @@
-import express from "express";
-import cors from "cors"
+import app from "./app.js"
 import dotenv from "dotenv"
+import connectDB from "./lib/db.js";
 
 // Configuring the environment variables
 dotenv.config();
 
-// Instantiating express
-const app = express();
-
-// Middlewares
-app.use(cors())
+connectDB();
 
 const PORT = process.env.PORT || 5000
 
