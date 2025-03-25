43ca76096f4944ec4210839111110dcfc97586da
Saipraveen49 <cheemakurthypraveen@gmail.com>
2025-03-25 18:27:16 +0530

    Challenges Backend Added

--- server/models/Challenge.js
+++ server/models/Challenge.js
@@ -3,14 +3,11 @@ import mongoose from "mongoose";
 const challengeSchema = new mongoose.Schema({
     title: { type: String, required: true, trim: true },
     description: { type: String, required: true },
-    category: [{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Category'
-    }],
+    category: [{ type: String, required: true }], // Change: Now an array of strings
     difficulty: { type: String, required: true, enum: ['Easy', 'Medium', 'Hard'] },
     points: { type: Number, required: true },
     problemLink: { type: String, required: true },
     createdAt: { type: Date, default: Date.now }
-})
+});
 
 export const Challenge = mongoose.model('Challenge', challengeSchema);
